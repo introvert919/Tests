2569d31bc5c72ca54c6aed3bb7e7e992fc8aead1
Andrey Fatkulin <146550720+introvert919@users.noreply.github.com>
2024-08-14 18:20:03 +0300

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using System.Threading.Tasks;
+
+namespace For_Jenkins
+{
+    [TestFixture]
+    public class For_Jenkins
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            string Actual = "a";
+            string Expected = "a";
+            ClassicAssert.AreEqual(Actual, Expected);   
+        }
+    }
+}
